cd220851a61b913e4b04ba4982550676e9fb2aeb
黄浒烨 <huanghuye@hymson.com>
2024-06-05 23:09:09 +0800

    添加项目文件。

---
+++ AYE-ClientSideWpf/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using AYE_ClientSideWpf.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace AYE_ClientSideWpf
+{
+    public class Bootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
