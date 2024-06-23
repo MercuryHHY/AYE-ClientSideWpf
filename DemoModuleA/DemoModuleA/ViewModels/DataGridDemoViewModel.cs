176e2882cf94ac87e6ab79372a4ca9020a987f2b
黄浒烨 <huanghuye@hymson.com>
2024-06-23 21:04:45 +0800

    简陋版本的数据表基本功能实现

--- DemoModuleA/DemoModuleA/ViewModels/DataGridDemoViewModel.cs
+++ DemoModuleA/DemoModuleA/ViewModels/DataGridDemoViewModel.cs
@@ -1,4 +1,10 @@
-﻿using AYE_Entity.Dtos;
+﻿using AYE_Entity;
+using AYE_Entity.Dtos;
+using AYE_Interface;
+using AYE_Service;
+using HandyControl.Tools.Command;
+using ImTools;
+using Prism.Ioc;
 using Prism.Mvvm;
 using SqlSugar;
 using System;
@@ -13,19 +19,26 @@ namespace DemoModuleA.ViewModels
 {
     public class DataGridDemoViewModel : BindableBase
     {
-
-        private ObservableCollection<DictionaryDto> dataList;
-
-        public ObservableCollection<DictionaryDto> DataList
+        private readonly ISuperRepository<DictionaryEntity> _DictionaryRepository;
+        private List<DictionaryEntity> dataList;
+        public AsyncCommand<string, bool> AsyncUpCommand { get; set; }
+        public List<DictionaryEntity> DataList
         {
             get { return dataList; }
             set { dataList = value; RaisePropertyChanged(); }
         }
-        public DataGridDemoViewModel()
+        public DataGridDemoViewModel(IContainerProvider containerProvider)
         {
-            DataList=new ObservableCollection<DictionaryDto>();
-            DataList.Add(new DictionaryDto() { DictType = "myDictType" , DictLabel = "myDictLabel" , DictValue = "myDictValue", Remark = "myRemark" });
+            _DictionaryRepository = new SuperRepository<DictionaryEntity>(containerProvider, DbType.MySql.ToString());
+            DataList = new List<DictionaryEntity>();
+            AsyncUpCommand = new AsyncCommand<string, bool>(ExecuteAsync);
+           
+        }
 
+        private async Task<bool> ExecuteAsync(string arg)
+        {
+            DataList = await _DictionaryRepository.GetListAsync();
+            return true;
         }
     }
 }
--- DemoModuleB/DemoModuleB/ViewModels/UserControlBViewModel.cs
+++ DemoModuleB/DemoModuleB/ViewModels/UserControlBViewModel.cs
@@ -16,12 +16,12 @@ namespace DemoModuleB.ViewModels;
 
 public class UserControlBViewModel
 {
-    private readonly ISuperRepository<UserInfo001Entity> _Userrepository;
+    private readonly ISuperRepository<DictionaryEntity> _DictionaryRepository;
     public AsyncCommand<string,bool> AsyncDBCommand { get; set; }
 
     public UserControlBViewModel(IContainerProvider containerProvider)
     {
-        _Userrepository = new SuperRepository<UserInfo001Entity>(containerProvider, DbType.MySql.ToString());
+        _DictionaryRepository = new SuperRepository<DictionaryEntity>(containerProvider, DbType.MySql.ToString());
 
         //注意 这里 HandyControl.Tools.Command中的 异步命令AsyncCommand 的使用与 prism 中的命令不同，要注意看源码中的定义
         AsyncDBCommand = new AsyncCommand<string, bool>(ExecuteAsync);
@@ -37,7 +37,12 @@ public class UserControlBViewModel
         try
         {
             // 模拟一个异步操作
-            await Task.Delay(20);
+            //await Task.Delay(20);
+            await _DictionaryRepository.InsertAsync(new DictionaryEntity() { DictType = "myDictType", DictLabel = "myDictLabel", DictValue = "myDictValue", Remark = "myRemark" });
+            await _DictionaryRepository.InsertAsync(new DictionaryEntity() { DictType = "myDictType", DictLabel = "myDictLabel", DictValue = "myDictValue", Remark = "myRemark" });
+            await _DictionaryRepository.InsertAsync(new DictionaryEntity() { DictType = "myDictType", DictLabel = "myDictLabel", DictValue = "myDictValue", Remark = "myRemark" });
+            await _DictionaryRepository.InsertAsync(new DictionaryEntity() { DictType = "myDictType", DictLabel = "myDictLabel", DictValue = "myDictValue", Remark = "myRemark" });
+            await _DictionaryRepository.InsertAsync(new DictionaryEntity() { DictType = "myDictType", DictLabel = "myDictLabel", DictValue = "myDictValue", Remark = "myRemark" });
             Console.WriteLine($"异步操作完成。所得参数是{op}");
         }
         catch (Exception ex)
