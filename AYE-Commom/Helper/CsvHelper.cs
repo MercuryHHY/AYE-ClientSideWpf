f92e6bcda7fab7d4f68b02007e4d34f218dd0cc8
黄浒烨 <huanghuye@hymson.com>
2024-09-20 17:59:39 +0800

    新增NPIO操作

---
+++ AYE-Commom/Helper/CsvHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYE_Commom.Helper;
+
+public class CsvHelper
+{
+    /// <summary>
+    /// 数据保存在CSV文件中，如果存在就续写，不存在则创建
+    /// </summary>
+    /// <param name="key">CSV文件头</param>
+    /// <param name="value">要写入的字符串，数据之间用英文逗号隔开</param>
+    /// <param name="strDirPath"> 要写入的文件路径</param>
+    /// <param name="strFileName">文件名</param>
+    /// <param name="bHasTitle">是否写入标题（也就是CSV文件头）</param>
+    /// <returns></returns>
+    public static bool CsvWriteFile(string key, string value, string strDirPath, string strFileName, bool bHasTitle)
+    {
+        try
+        {
+            if (!Directory.Exists(strDirPath))
+            {
+                Directory.CreateDirectory(strDirPath);
+            }
+
+            string path = Path.Combine(strDirPath, strFileName);
+            bool flag = !File.Exists(path);
+            using (StreamWriter streamWriter = new StreamWriter(path, append: true, Encoding.UTF8))
+            {
+                if (bHasTitle && flag)
+                {
+                    streamWriter.WriteLine(key);
+                }
+
+                streamWriter.WriteLine(value);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public static DataTable OpenCSV(string filePath, out string strError)
+    {
+        strError = "";
+        try
+        {
+            DataTable dataTable = new DataTable();
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fileStream, Encoding.Default);
+            string text = "";
+            string[] array = null;
+            string[] array2 = null;
+            int num = 0;
+            bool flag = true;
+            while ((text = streamReader.ReadLine()) != null)
+            {
+                if (flag)
+                {
+                    array2 = text.Split(',');
+                    flag = false;
+                    num = array2.Length;
+                    for (int i = 0; i < num; i++)
+                    {
+                        DataColumn column = new DataColumn(array2[i]);
+                        dataTable.Columns.Add(column);
+                    }
+                }
+                else
+                {
+                    array = text.Split(',');
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int j = 0; j < num; j++)
+                    {
+                        dataRow[j] = array[j].Trim();
+                    }
+
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+
+            streamReader.Close();
+            fileStream.Close();
+            return dataTable;
+        }
+        catch (Exception ex)
+        {
+            strError = ex.ToString();
+            return null;
+        }
+    }
+
+    public static bool DataTableToCSV(string filePath, DataTable dt, out string strError)
+    {
+        strError = "";
+        try
+        {
+            string directoryName = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            using (StreamWriter streamWriter = new StreamWriter(filePath, append: false, Encoding.UTF8))
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    if (i != 0)
+                    {
+                        stringBuilder.Append(",");
+                    }
+
+                    stringBuilder.Append(dt.Columns[i].ColumnName);
+                }
+
+                streamWriter.WriteLine(stringBuilder);
+                stringBuilder.Clear();
+                foreach (DataRow row in dt.Rows)
+                {
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        if (j != 0)
+                        {
+                            stringBuilder.Append(",");
+                        }
+
+                        if (dt.Columns[j].DataType == typeof(string) && row[dt.Columns[j]].ToString().Contains(","))
+                        {
+                            stringBuilder.Append("\"" + row[dt.Columns[j]].ToString().Replace("\"", "\"\"") + "\"");
+                        }
+                        else
+                        {
+                            stringBuilder.Append(row[dt.Columns[j]].ToString());
+                        }
+                    }
+
+                    streamWriter.WriteLine(stringBuilder);
+                    stringBuilder.Clear();
+                }
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            strError = ex.Message;
+            return false;
+        }
+    }
+}
---
+++ AYE-Commom/Helper/ExcelHelper.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.UserModel;
+using System.Data;
+using System.IO;
+using NPOI.XSSF.UserModel;
+using NPOI;
+using SqlSugar;
+
+namespace AYE_Commom.Helper
+{
+    public static class ExcelHelper
+    {
+        /// <summary>
+        /// 将Excel转化成DataTable
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static DataTable ReadExcelToTable(string fileName, Stream stream)
+        {
+            using (stream)
+            {
+                IWorkbook workbook = null;
+                if (fileName.EndsWith(".xlsx"))// 2007版本
+                {
+                    workbook = new XSSFWorkbook(stream);
+                }
+                else// 2003版本
+                {
+                    workbook = new HSSFWorkbook(stream);
+                }
+                //var workbook = new XSSFWorkbook(tempFile);
+                var sheet = workbook.GetSheetAt(0);
+                var dataTable = new DataTable();
+                var tableHeadRow = sheet.GetRow(0);
+                for (int i = 0; i < tableHeadRow.PhysicalNumberOfCells; i++)
+                {
+                    var headCell = tableHeadRow.Cells[i];
+                    dataTable.Columns.Add(new DataColumn(headCell.StringCellValue));
+                }
+                for (int i = 1; i < sheet.PhysicalNumberOfRows; i++)
+                {
+                    var row = sheet.GetRow(i);
+                    var newRow = dataTable.NewRow();
+                    // for (int j = 0; j < row.PhysicalNumberOfCells; j++)
+                    for (int j = 0; j < tableHeadRow.PhysicalNumberOfCells; j++)
+                    {
+                        if (row.GetCell(j) == null)
+                        {
+                            continue;
+                        }
+                        //var cell = row.Cells[j];
+                        var cell = row.GetCell(j);
+                        if (cell.CellType == CellType.String)
+                        {
+                            newRow[j] = cell.StringCellValue.Trim();
+                        }
+                        else if (cell.CellType == CellType.Numeric)
+                        {
+                            newRow[j] = cell.NumericCellValue;
+                        }
+                        else if (cell.CellType == CellType.Formula)
+                        {
+                            try
+                            {
+                                newRow[j] = cell.StringCellValue.ToString().Trim();
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            newRow[j] = cell.StringCellValue;
+                        }
+                    }
+                    dataTable.Rows.Add(newRow);
+                }
+                // workbook.Clear();
+                workbook.Close();
+                return dataTable;
+            }
+        }
+
+        /// <summary>
+        /// 将Excel转化成DataTable
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static DataTable ReadExcelToTable(string fileName, Stream stream, string sheetName)
+        {
+            using (stream)
+            {
+                IWorkbook workbook = null;
+                if (fileName.EndsWith(".xlsx"))// 2007版本
+                {
+                    workbook = new XSSFWorkbook(stream);
+                }
+                else// 2003版本
+                {
+                    workbook = new HSSFWorkbook(stream);
+                }
+                //var workbook = new XSSFWorkbook(tempFile);
+                //var sheet = workbook.GetSheetAt(0);
+                var sheet = workbook.GetSheet(sheetName);
+                var dataTable = new DataTable();
+                if (sheet == null)
+                {
+                    return dataTable;
+                }
+
+                var tableHeadRow = sheet.GetRow(0);
+                for (int i = 0; i < tableHeadRow.PhysicalNumberOfCells; i++)
+                {
+                    var headCell = tableHeadRow.Cells[i];
+                    dataTable.Columns.Add(new DataColumn(headCell.StringCellValue));
+                }
+                for (int i = 1; i < sheet.PhysicalNumberOfRows; i++)
+                {
+                    var row = sheet.GetRow(i);
+                    if (row == null)
+                    {
+                        continue;
+                    }
+                    var newRow = dataTable.NewRow();
+                    // for (int j = 0; j < row.PhysicalNumberOfCells; j++)
+                    for (int j = 0; j < tableHeadRow.PhysicalNumberOfCells; j++)
+                    {
+                        if (row.GetCell(j) == null)
+                        {
+                            continue;
+                        }
+                        //var cell = row.Cells[j];
+                        var cell = row.GetCell(j);
+                        if (cell.CellType == CellType.String)
+                        {
+                            newRow[j] = cell.StringCellValue.Trim();
+                        }
+                        else if (cell.CellType == CellType.Numeric)
+                        {
+                            newRow[j] = cell.NumericCellValue;
+                        }
+                        else if (cell.CellType == CellType.Formula)
+                        {
+                            try
+                            {
+                                newRow[j] = cell.StringCellValue.ToString().Trim();
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            newRow[j] = cell.StringCellValue;
+                        }
+                    }
+                    dataTable.Rows.Add(newRow);
+                }
+                // workbook.Clear();
+                workbook.Close();
+                return dataTable;
+            }
+        }
+
+        /// <summary>
+        /// 将datatable转化为Excel
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        public static string DataTableToExcel(DataTable dataTable, string filePath)
+        {
+            var dt = dataTable;
+            using (var fileStream = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                var workBook = new XSSFWorkbook();
+                var sheet = workBook.CreateSheet();
+
+                IRow headRow = sheet.CreateRow(0);
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    ICell cell = headRow.CreateCell(i);
+                    cell.SetCellValue(dt.Columns[i] == null ? "" : dt.Columns[i].ToString());
+                }
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        IRow newRow = sheet.CreateRow(i + 1);
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            ICell cell = newRow.CreateCell(j);
+                            cell.SetCellValue(dt.Rows[i][j] == null ? "" : dt.Rows[i][j].ToString());
+                        }
+                    }
+                }
+                workBook.Write(fileStream);
+                workBook.Clear();
+                workBook.Close();
+            }
+            return filePath;
+        }
+
+        /// <summary>
+        /// 创建Excel文件，并返回IWorkbook
+        /// 一般与下面的方法配合使用，
+        /// 先创建，然后用下面的函数读取Excel数据
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static IWorkbook CreateWorkbook(Stream stream)
+        {
+            return WorkbookFactory.Create(stream);
+        }
+
+
+        /// <summary>
+        /// 将Excel转化成DataTable
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static DataTable ReadExcelToTable(IWorkbook workbook, string sheetName)
+        {
+            var sheet = workbook.GetSheet(sheetName);
+            var dataTable = new DataTable();
+            if (sheet == null)
+            {
+                return dataTable;
+            }
+
+            var tableHeadRow = sheet.GetRow(0);
+            for (int i = 0; i < tableHeadRow.PhysicalNumberOfCells; i++)
+            {
+                var headCell = tableHeadRow.Cells[i];
+                dataTable.Columns.Add(new DataColumn(headCell.StringCellValue));
+            }
+
+            for (int i = 1; i < sheet.PhysicalNumberOfRows; i++)
+            {
+                var row = sheet.GetRow(i);
+                if (row == null)
+                {
+                    continue;
+                }
+                var newRow = dataTable.NewRow();
+                for (int j = 0; j < tableHeadRow.PhysicalNumberOfCells; j++)
+                {
+                    if (row.GetCell(j) == null)
+                    {
+                        continue;
+                    }
+                    var cell = row.GetCell(j);
+                    if (cell.CellType == CellType.String)
+                    {
+                        newRow[j] = cell.StringCellValue.Trim();
+                    }
+                    else if (cell.CellType == CellType.Numeric)
+                    {
+                        newRow[j] = cell.NumericCellValue;
+                    }
+                    else if (cell.CellType == CellType.Formula)
+                    {
+                        try
+                        {
+                            newRow[j] = cell.StringCellValue.ToString().Trim();
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        newRow[j] = cell.StringCellValue;
+                    }
+                }
+                dataTable.Rows.Add(newRow);
+            }
+            return dataTable;
+        }
+   
+    
+    }
+
+}
