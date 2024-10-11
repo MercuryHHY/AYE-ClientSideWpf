2ac626ab4ff4734bc1a9ecb756e83a71f648f75f
MercuryHHY <3294084766@qq.com>
2024-10-11 17:09:34 +0800

    暂存，开始动手

---
+++ AYE-BaseFramework/AYE.BaseFramework.Manager/BaseFrameworkManagerModule.cs
@@ -0,0 +1,33 @@
+﻿using AYE.BaseFramework.QuartzCore;
+using AYE.BaseFramework.SqlSusgarCore;
+using AYE_BaseFramework.ConfigurationCore;
+using Microsoft.Extensions.Logging;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using Quartz.Impl;
+using Quartz;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AYE.BaseFramework.Manager;
+
+public class BaseFrameworkManagerModule : IModule
+{
+   
+
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        throw new NotImplementedException();
+    }
+}
